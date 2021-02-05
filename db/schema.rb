a33479193f415f31ebcb7d0b2717513ca9c09f4d
Ramona Maltan <ramonamaltan@gmail.com>
2021-02-05 10:59:00 +0100

    created initial models and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_094115) do
+ActiveRecord::Schema.define(version: 2021_02_05_095655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_categories_on_recipe_id"
+  end
+
+  create_table "doses", force: :cascade do |t|
+    t.string "unit"
+    t.integer "amount"
+    t.bigint "recipe_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_doses_on_recipe_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +61,8 @@ ActiveRecord::Schema.define(version: 2021_02_05_094115) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "recipes"
+  add_foreign_key "doses", "ingredients"
+  add_foreign_key "doses", "recipes"
+  add_foreign_key "recipes", "users"
 end
