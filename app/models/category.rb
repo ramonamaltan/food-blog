1e1e22c3d9bd63913626beb5b99582da42e8abbc
Ramona Maltan <ramonamaltan@gmail.com>
2021-02-05 11:44:43 +0100

    added category model tests and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   belongs_to :recipe
+
+  validates :name, presence: true
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @recipe = recipes(:one)
+  end
+  
+  test "valid category must have name and recipe" do
+    category = Category.new(name: 'vegan', recipe: @recipe)
+    assert category.valid?
+  end
+
+  test 'category must have a name to be valid' do
+    category = Category.new(recipe: @recipe)
+    assert_not category.valid?
+  end
 end
