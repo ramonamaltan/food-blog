1dc2a5cbe2aff69a855ab893d3790702c5e5d1ce
Ramona Maltan <ramonamaltan@gmail.com>
2021-02-05 11:40:05 +0100

    added recipe model tests and validatations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  validates :name, uniqueness: true
+  validates :name, presence: true
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test 'recipe without name should be invalid' do
+    recipe = Recipe.new(user: @user)
+    assert_not recipe.valid?
+  end
+
+  test 'recipe with taken name should be invalid' do
+    other_recipe = recipes(:one)
+    recipe = Recipe.new(name: other_recipe.name, user: @user)
+
+    assert_not recipe.valid?
+  end
 end
