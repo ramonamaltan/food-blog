07561bf00dcc78e53f8563a9b5f7da2bd52222f4
Ramona Maltan <ramonamaltan@gmail.com>
2021-02-05 11:28:17 +0100

    test user model, added validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :recipes, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'user with taken email should be invalid' do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, encrypted_password: 'test123456')
+    assert_not user.valid?
+  end
+
+  test 'destroy user should destroy all recipes of this user' do
+    assert_difference('Recipe.count', -1) do
+      users(:one).destroy
+    end
+  end
 end
