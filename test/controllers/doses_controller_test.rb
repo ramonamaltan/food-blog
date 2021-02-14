f78b8adce7f274bb711ce5e8646d4c669bc0f653
Ramona Maltan <ramonamaltan@gmail.com>
2021-02-14 21:15:16 +0100

    status quo

--- test/controllers/doses_controller_test.rb
+++ test/controllers/doses_controller_test.rb
@@ -18,7 +18,7 @@ class DosesControllerTest < ActionDispatch::IntegrationTest
     login_as users(:one)
 
     assert_difference("Dose.count") do
-      post recipe_doses_url(@recipe), params: { dose: { unit: 'grams', amount: 200, recipe_id: @recipe.id, ingredient_attributes: [2, 'something'] } }
+      post recipe_doses_url(@recipe), params: { dose: { unit: 'grams', amount: 200, recipe_id: @recipe.id, ingredient_attributes: { id: @ingredient.id, name: @ingredient.name } } }
       puts assigns(:dose).errors.full_message
     end
   end
