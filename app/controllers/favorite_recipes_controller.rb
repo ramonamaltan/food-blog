6c72379253f81dfd16b763ecc55fc9b20aa5d81c
Ramona Maltan <ramonamaltan@gmail.com>
2021-02-19 11:09:49 +0100

    create or destroy favorite recipe toggle

--- app/controllers/favorite_recipes_controller.rb
+++ app/controllers/favorite_recipes_controller.rb
@@ -2,6 +2,12 @@ class FavoriteRecipesController < ApplicationController
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
+    favorite_exists = FavoriteRecipe.where(recipe: @recipe, user: current_user)
+    authorize favorite_exists
+    if favorite_exists.count.positive?
+      favorite_exists.destroy_all
+      return
+    end
     @favorite_recipe = FavoriteRecipe.new(recipe: @recipe, user: current_user)
     authorize @favorite_recipe
     if @favorite_recipe.save
