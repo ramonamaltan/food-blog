afadbaf129c00e5524caab010fd7da202ddf0dee
Ramona Maltan <ramonamaltan@gmail.com>
2021-02-23 17:14:33 +0100

    fix admin error

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   before_action :find_recipe, only: [:edit, :update, :destroy]
   def index
-    @admin = current_user.admin == true
+    @admin = !current_user.nil? && current_user.admin == true
     @recipes = policy_scope(Recipe).where(nil)
     @recipes = Recipe.search_by_name(params[:search]) if params[:search].present?
     @recipes = Recipe.sort_by_most_recent if params[:recent].present?
@@ -10,7 +10,7 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @admin = current_user.admin == true
+    @admin = !current_user.nil? && current_user.admin == true
     @recipe = Recipe.includes(:doses, :ingredients).find(params[:id])
     authorize @recipe
     @favorite_recipe = FavoriteRecipe.new
